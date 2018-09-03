2698f3163226d2f4d2426aa131fab4d7444c0a42
Andrew Oh <andrewoh531@gmail.com>
2018-09-03 13:15:40 +1000

    Refactoring library to use for https://notify.gov.au

---
+++ src/main/java/au/gov/notify/NotificationClientException.java
@@ -0,0 +1,33 @@
+package au.gov.notify;
+
+public class NotificationClientException extends Exception
+{
+    private static final long serialVersionUID = 2l;
+    private int httpResult;
+
+    public NotificationClientException(Exception ex)
+    {
+        super(ex);
+    }
+
+    public NotificationClientException(String message)
+    {
+        super(message);
+    }
+
+    public NotificationClientException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+
+    NotificationClientException(int httpResult, String message)
+    {
+        super("Status code: " + httpResult + " " + message);
+        this.httpResult = httpResult;
+    }
+
+    public int getHttpResult()
+    {
+        return this.httpResult;
+    }
+}
